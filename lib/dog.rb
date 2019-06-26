2ab653411f7290da1be79feb0ee8ad0fdcc24856
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-06-26 13:50:11 -0500

    done

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    def initialize
+        @Dog
+    end
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+fido.bark
+fido.sit
